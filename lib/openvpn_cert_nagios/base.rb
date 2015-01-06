d436228aa694b5e40a58e6183651cc5b931d9f90
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2015-01-06 11:53:57 +0900

    remove unused

--- lib/openvpn_cert_nagios/base.rb
+++ lib/openvpn_cert_nagios/base.rb
@@ -2,9 +2,6 @@
 
 require "nagios_check"
 require 'open3'
-require 'optparse'
-require 'time'
-require 'active_support/core_ext'
 
 require "openvpn_cert_nagios/shell"
 require "openvpn_cert_nagios/certs"
