b5dab5d413b0306c8c1e4b3ca43ab28118d8da9c
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2015-01-08 16:08:39 +0900

    Simplify logic of Shell.exe, remove unused cases.

--- lib/openvpn_cert_nagios/shell.rb
+++ lib/openvpn_cert_nagios/shell.rb
@@ -4,22 +4,15 @@ class CertCheck
   # STDOUT or exit status, and aborts on non zero status.
   #
   class Shell
-    def self.exe(cmd, ignore_status: false, only_status: false)
+    def self.exe(cmd)
 
       puts "DEBUG: #{cmd}" if $debug
       Open3.popen3({"PATH" => "/usr/bin:/bin:/usr/sbin:/sbin"}, cmd) do |stdin, stdout, stderr, process|
 
         stdin.close
-        case
-        when only_status
-          process.value
-        when ignore_status
-          stdout.readlines
-        when process.value != 0
-          abort "#{cmd} FAILED: #{stderr.read} #{ stdout.read }"
-        else
-          stdout.readlines
-        end
+        raise "#{cmd} FAILED: #{stderr.read} #{ stdout.read }" if process.value != 0
+        stdout.readlines
+
       end
     end
   end
